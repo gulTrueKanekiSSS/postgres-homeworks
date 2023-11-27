52011f8827163cbbddb626a6dde109cdc6986c32
Дмитрий Захаров <zds.vip1221@gmail.com>
2023-11-27 15:38:13 +0300

    qwerty

--- homework-2/groupby.sql
+++ homework-2/groupby.sql
@@ -18,3 +18,31 @@
 
 
 -- 7. страны, в которых зарегистрированы и заказчики (customers) и поставщики (suppliers), но не зарегистрированы работники (employees).
+
+SELECT DISTINCT(ship_city, ship_country) FROM orders WHERE ship_city LIKE '%burg';
+
+SELECT order_id, customer_id, freight, ship_country FROM orders WHERE ship_country LIKE 'P%' ORDER BY freight DESC LIMIT 10;
+
+SELECT last_name, first_name, home_phone FROM employees WHERE region IS NULL;
+
+SELECT country, COUNT(*) AS amount FROM suppliers GROUP BY country ORDER BY amount DESC;
+
+SELECT ship_country, SUM(freight) as sum_freights FROM orders WHERE ship_region IS NOT NULL  GROUP BY ship_country HAVING SUM(freight) > 2750 ORDER BY sum_freights DESC;
+
+SELECT country
+FROM suppliers
+INTERSECT
+SELECT country
+FROM customers
+INTERSECT
+SELECT country
+FROM employees;
+
+SELECT country
+FROM suppliers
+INTERSECT
+SELECT country
+FROM customers
+EXCEPT
+SELECT country
+FROM employees
