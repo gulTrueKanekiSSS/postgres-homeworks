c8899757615c0ff483659e98e60acf959c9988c5
Дмитрий Захаров <zds.vip1221@gmail.com>
2023-11-27 12:13:28 +0300

    qwerty

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,30 @@
+
 -- SQL-команды для создания таблиц
+CREATE DATABASE north
+
+CREATE TABLE employees
+(
+	employee_id serial PRIMARY KEY,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100),
+	birth_date varchar(100),
+	notes text
+);
+
+CREATE TABLE customers
+(
+	customer_id varchar(100) PRIMARY KEY,
+	company_name varchar(50),
+	contact_name varchar(50)
+);
+
+CREATE TABLE orders
+(
+	order_id serial PRIMARY KEY,
+	customer_id varchar(100) REFERENCES customers(customer_id),
+	employee_id int NOT NULL REFERENCES employees(employee_id),
+	order_date date NOT NULL,
+	ship_city varchar(100)
+);
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,40 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+connection = psycopg2.connect(
+    database="north",
+    host="localhost",
+    user="postgres",
+    password="12345",
+    port="5432"
+)
+
+cursor = connection.cursor()
+
+with open("north_data/customers_data.csv", 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cursor.execute("INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+                    (row[0], row[1], row[2]))
+
+
+with open("north_data/employees_data.csv", 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cursor.execute("INSERT INTO employees (first_name, last_name, title, birth_date, notes) VALUES (%s, %s, %s, %s, %s)",
+                    (row[0], row[1], row[2], row[3], row[4]))
+
+with open("north_data/orders_data.csv", 'r') as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        cursor.execute("INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+                    (row[0], row[1], row[2], row[3], row[4]))
+
+
+connection.commit()
+connection.close()
